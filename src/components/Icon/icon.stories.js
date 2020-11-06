904b4923316dee896e80b0e76d3779cb945ae47f
gogo-code <3059349417@qq.com>
2020-11-06 23:34:50 +0800

    icon组件完成

--- src/components/Icon/icon.stories.js
+++ src/components/Icon/icon.stories.js
@@ -1,10 +1,40 @@
-import React from "react";
-import Icon from ".";
-
+import React from 'react';
+import Icon from '.';
+// 起别名
+import { ReactComponent as SmileIcon } from 'assets/icons/smile.svg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faCommentDots,
+  faFolder,
+  faStickyNote,
+} from '@fortawesome/free-solid-svg-icons';
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-  title: "UI 组件/Icon",
-  component: Icon
+  title: 'UI 组件/Icon',
+  component: Icon,
 };
 
-export const Default = () => <Icon/>;
+export const Default = () => <Icon icon={SmileIcon} />;
+
+export const CustomColor = () => {
+  return <Icon icon={SmileIcon} color="#ccc" />;
+};
+
+export const CustomColorSize = () => {
+  return <Icon icon={SmileIcon} width={48} height={48} color="#ccc" />;
+};
+export const FontAwesome = () => {
+  return <FontAwesomeIcon icon={faCommentDots} />;
+};
+export const FontAwesomeColor = () => {
+  return <FontAwesomeIcon icon={faCommentDots} style={{ color: '#cccccc' }} />;
+};
+export const FontAwesomeSizes = () => {
+  return (
+    <div>
+      <FontAwesomeIcon icon={faFolder} style={{ fontSize: '24px' }} />
+      <FontAwesomeIcon icon={faStickyNote} style={{ fontSize: '36px' }} />
+      <FontAwesomeIcon icon={faCommentDots} style={{ fontSize: '48px' }} />
+    </div>
+  );
+};
--- src/components/Icon/index.js
+++ src/components/Icon/index.js
@@ -3,17 +3,27 @@ import PropTypes from 'prop-types';
 import StyledIcon from './style';
 
 function Icon({
-  
+  icon: IconComponent,
+  width = 24,
+  height = 24,
+  color,
+  opacity,
   ...rest
 }) {
   return (
-    <StyledIcon {...rest}>
-      hello world
+    // 将props传入到组件中
+    <StyledIcon color={color} opacity={opacity} {...rest}>
+      {IconComponent && <IconComponent width={width} height={height} />}
     </StyledIcon>
   );
 }
- Icon.propTypes = {
-  
+Icon.propTypes = {
+  // 接收组件
+  icon:PropTypes.element,
+  width:PropTypes.oneOfType([PropTypes.number,PropTypes.string]),
+  height:PropTypes.oneOfType([PropTypes.number,PropTypes.string]),
+  color:PropTypes.string,
+  opacity:PropTypes.number
 };
 
 export default Icon;
